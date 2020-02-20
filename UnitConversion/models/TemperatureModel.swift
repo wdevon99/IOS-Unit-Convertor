48d2ce30a6b0e75ac37c8d6163e3c96833fe1e2b
Devon Wijesinghe <wdevon99@gmail.com>
2020-02-20 09:49:39 +0530

    Adds folders

---
+++ UnitConversion/models/TemperatureModel.swift
@@ -0,0 +1,10 @@
+//
+//  TemperatureModel.swift
+//  UnitConversion
+//
+//  Created by Devon Wijesinghe on 2/20/20.
+//  Copyright © 2020 Devon Wijesinghe. All rights reserved.
+//
+
+import Foundation
+
